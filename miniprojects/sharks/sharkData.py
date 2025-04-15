44c67cfe93c0cd87b245427e847f03595b9fd36d
Alexander Lolk <Lolk@itu.dk>
2025-04-15 13:05:08 +0200

    start miniproject for sharks

--- miniprojects/sharks/sharkData.py
+++ miniprojects/sharks/sharkData.py
@@ -12,6 +12,54 @@ import numpy as np
 # from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 
+# - Date: Date of the incident
+# - Year: Year of the incident
+# - Type: Whether the incident was provoked (i.e. the human involved speared, hooked or captured the shark first), unprovoked (e.g. when the shark bit out of curiosity), involved a watercraft (when the shark bit or rammed a boat), involved a disaster (i.e. a capsizing boat or a crashing plane) or is questionable (i.e. there is not enough data to determine whether a shark was involved in the actual incident)
+# - Country: The country the incident happened in
+# - State: The state in the country in which the incident happened
+# - Location: The placename of the area in which the incident happened
+# - Activity: The activity of the human immediately before the incident
+# - Name: The name of the human involved in the incident
+# - Sex: The gender of the human involved in the incident
+# - Age: The age of the human involved in the incident
+# - Injury: A textual description of the resulting injury to the human
+# - Unnamed: 11: Unexplained data
+# - Time: Time of day of the incident
+# - Species: Species of the shark involved in the incident
+# - Source: Source of the reporting of the incident
+# - pdf: Filename of the report of the incident
+# - href formula: Links to reports, no longer functional
+# - href: Same as above
+# - Case Number: Internal reference
+# - Case Number.1: Same as above
+# - original order: Ordering in the original system
+# - Unnamed: 21: Unknown data                                       - Removed
+# - Unnamed: 22: Unknown data                                       - Removed
+# - fatal: Whether the incident resulted in the human dying
+
 df = pd.read_csv("dataExam/shark-incidents.csv")
 
-print(df)
+# print(df.describe)
+
+def check_case_nan():
+    filt = df[["Case Number", "Case Number.1"]]
+    mask = filt.isna().any(axis=1)
+    # print(mask.value_counts())
+    # print(df[mask])
+    
+    # 
+    nan_count_per_row = df[mask].isna().sum(axis=1)
+    # print(nan_count_per_row)
+    return df[df.isna().sum(axis=1) < 20]
+# removes rows which has almost nothing in them
+df = check_case_nan()
+
+def check_Unamed_nan():
+    mask = df[["Unnamed: 11", "Unnamed: 21", "Unnamed: 22"]].isna().any(axis=1)
+    # print(mask.value_counts())
+    return df.drop(columns=["Unnamed: 21", "Unnamed: 22"])
+# removes column which are almost not used
+df = check_Unamed_nan()
+
+# print(df["href"].value_counts())
+
